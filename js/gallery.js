781d5609aa81c2cc7f35f336732778b3707d1937
jodiema <jodiemaxf@gmail.com>
2020-06-29 14:17:45 -0700

    collapsible tabs

--- js/gallery.js
+++ js/gallery.js
@@ -15,3 +15,18 @@ function openCity(evt, cityName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
